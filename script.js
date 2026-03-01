0fef28c244904a8326d3ae5ec245a14f24d53504
maxton480 <maxton480@gmail.com>
2026-03-01 22:25:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,88 @@
 const input=document.getElementById("images");
-const controls=document.getElementById("controls");
+const list=document.getElementById("imageList");
 
 let images=[];
 let crops=[];
 
 input.onchange=async()=>{
-  controls.innerHTML="";
-  images=[];
-  crops=[];
 
-  for(let file of input.files){
+list.innerHTML="";
+images=[];
+crops=[];
 
-    const img=new Image();
-    img.src=URL.createObjectURL(file);
+for(let file of input.files){
 
-    await new Promise(r=>img.onload=r);
+const img=new Image();
+img.src=URL.createObjectURL(file);
+await new Promise(r=>img.onload=r);
 
-    images.push(img);
-    crops.push(0);
+images.push(img);
+crops.push(0);
 
-    createSlider(images.length-1,img.height);
-  }
+createCard(img,images.length-1);
+
+}
 };
 
-function createSlider(index,max){
+function createCard(img,index){
 
-  const div=document.createElement("div");
-  div.className="sliderBox";
+const div=document.createElement("div");
+div.className="card";
 
-  div.innerHTML=`
-    Image ${index+1} Crop Top:
-    <input type="range" min="0" max="${max/2}" value="0"
-    oninput="updateCrop(${index},this.value)">
-  `;
+div.innerHTML=`
+<img src="${img.src}">
+<label>Crop repeated top part</label>
+<input type="range" min="0" max="${img.height/2}" value="0"
+oninput="updateCrop(${index},this.value)">
+`;
 
-  controls.appendChild(div);
+list.appendChild(div);
 }
 
 function updateCrop(i,val){
-  crops[i]=parseInt(val);
+crops[i]=parseInt(val);
 }
 
 function merge(){
 
-  if(!images.length) return;
+if(!images.length)return;
 
-  let width=images[0].width;
-  let height=0;
+let width=images[0].width;
+let height=0;
 
-  images.forEach((img,i)=>{
-    height+=img.height-crops[i];
-  });
+images.forEach((img,i)=>{
+height+=img.height-crops[i];
+});
 
-  const canvas=document.getElementById("canvas");
-  const ctx=canvas.getContext("2d");
+const canvas=document.getElementById("canvas");
+const ctx=canvas.getContext("2d");
 
-  canvas.width=width;
-  canvas.height=height;
+canvas.width=width;
+canvas.height=height;
 
-  let y=0;
+let y=0;
 
-  images.forEach((img,i)=>{
+images.forEach((img,i)=>{
 
-    ctx.drawImage(
-      img,
-      0,crops[i],
-      img.width,
-      img.height-crops[i],
-      0,y,
-      img.width,
-      img.height-crops[i]
-    );
+ctx.drawImage(
+img,
+0,crops[i],
+img.width,
+img.height-crops[i],
+0,y,
+img.width,
+img.height-crops[i]
+);
 
-    y+=img.height-crops[i];
-  });
+y+=img.height-crops[i];
 
-  canvas.toBlob(blob=>{
-    const url=URL.createObjectURL(blob);
-    const d=document.getElementById("download");
-    d.href=url;
-    d.style.display="block";
-  },"image/webp",0.95);
+});
+
+canvas.toBlob(blob=>{
+const url=URL.createObjectURL(blob);
+const d=document.getElementById("download");
+d.href=url;
+d.style.display="block";
+},"image/webp",0.95);
+
 }
